8595723151d76b28385d03479790d737d98d7be8
ejbarra <erikjbarra@gmail.com>
2025-08-19 16:19:24 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,5 @@
-# server/djangoapp/models.py
-
 # Uncomment the following imports before adding the Model code
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
@@ -11,18 +8,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Additional optional fields
     country = models.CharField(max_length=50, blank=True, null=True)
     founded = models.IntegerField(blank=True, null=True)
-    
     def __str__(self):
         return self.name
-
-
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
@@ -42,17 +35,14 @@ class CarModel(models.Model):
         ('COUPE', 'Coupe'),
         ('HATCHBACK', 'Hatchback'),
     ]
-    
     # Many-to-One relationship with CarMake
     car_make = models.ForeignKey(
         CarMake, 
         on_delete=models.CASCADE,
         related_name='car_models'
     )
-    
     # Dealer ID - refers to dealer in Cloudant database
     dealer_id = models.IntegerField()
-    
     # Required fields
     name = models.CharField(max_length=100)
     type = models.CharField(
@@ -66,10 +56,10 @@ class CarModel(models.Model):
             MaxValueValidator(2023)
         ]
     )
-    
     # Additional optional fields
     color = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2, blank=True, null=True)
     
     def __str__(self):
         return f"{self.car_make.name} {self.name}"
